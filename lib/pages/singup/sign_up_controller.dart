4de2dfce464015c04d7a2ceb27ca1173fcff1138
fragilebanana16 <850604714@qq.com>
2024-05-13 22:01:36 +0800

    删除firebase移动端请求api
    注意移动端地址要设置ip为局域网ip

--- lib/pages/singup/sign_up_controller.dart
+++ lib/pages/singup/sign_up_controller.dart
@@ -1,16 +1,24 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_course/common/global_loader.dart/global_loader.dart';
 import 'package:flutter_course/common/widgets/popup_messages.dart';
 import 'package:flutter_course/pages/singup/notifier/register_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 
 class SignUpController {
   late WidgetRef ref;
   SignUpController({required this.ref});
 
   Future<void> handleSignUp() async {
+    http.Response res = await http.post(
+      Uri.parse('http://192.168.0.107:8080/auth/test'),
+      body: '',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    print(res);
     var state = ref.read(registerNotiferProvider);
     String name = state.userName;
     String email = state.email;
@@ -43,26 +51,26 @@ class SignUpController {
     }
 
     ref.read(apploaderProvider.notifier).setLoaderValue(true);
-    Future.delayed(const Duration(seconds: 2), () async {
-      try {
-        var ctx = Navigator.of(ref.context);
-        final credential = await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(email: email, password: password);
-        if (kDebugMode) {
-          print(credential);
-        }
+    // Future.delayed(const Duration(seconds: 2), () async {
+    //   try {
+    //     var ctx = Navigator.of(ref.context);
+    //     final credential = await FirebaseAuth.instance
+    //         .createUserWithEmailAndPassword(email: email, password: password);
+    //     if (kDebugMode) {
+    //       print(credential);
+    //     }
 
-        if (credential.user != null) {
-          await credential.user?.sendEmailVerification();
-          await credential.user?.updateDisplayName(name);
-          toastInfo("Success!");
-          ctx.pop();
-        }
-      } catch (e) {
-        toastInfo("Err! $e");
-      }
+    //     if (credential.user != null) {
+    //       await credential.user?.sendEmailVerification();
+    //       await credential.user?.updateDisplayName(name);
+    //       toastInfo("Success!");
+    //       ctx.pop();
+    //     }
+    //   } catch (e) {
+    //     toastInfo("Err! $e");
+    //   }
 
-      ref.read(apploaderProvider.notifier).setLoaderValue(false);
-    });
+    //   ref.read(apploaderProvider.notifier).setLoaderValue(false);
+    // });
   }
 }
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 
 const catchAsync = require("../util/catchAsync");
 exports.test = catchAsync(async (req, res, next) => {
+  console.log('test')
     return res.status(400).json({
       status: "test status",
       message: "test test",
